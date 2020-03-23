82fe26191072c770bd5d1b81ef7be546b9527dd9
Luis <luiscarlos.sf@outlook.com>
2020-03-23 16:24:39 -0300

    Atualiza nome de classe

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Brand, Category, Computer, Equipament, Floor, Model, UA
+from .models import Brand, Category, Computer, Equipament, Floor, Model, Ua
 # Register your models here.
-admin.site.register([Brand, Category, Computer, Equipament, Floor, Model, UA])
+admin.site.register([Brand, Category, Computer, Equipament, Floor, Model, Ua])
