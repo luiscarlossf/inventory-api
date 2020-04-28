2cc44c089f7645e3b54c3a2b3ccc8322ebd194f6
Luis <luiscarlos.sf2017@gmail.com>
2020-04-28 16:07:58 -0300

    Adiciona esquema de versionamento a API.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,22 @@
-from django.urls import path
+from rest_framework.urlpatterns import url, include
+from rest_framework import routers 
+from rest_framework.settings import api_settings
+from .views import BrandViewSet, CategoryViewSet, ComputerViewSet, EquipamentViewSet, \
+    FloorViewSet, GroupViewSet, ModelViewSet, UaViewSet, UserViewSet, FileUploadViewSet
 
-from . import views
+router = routers.DefaultRouter()
+router.register(r'brands', BrandViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'computers', ComputerViewSet)
+router.register(r'equipaments', EquipamentViewSet)
+router.register(r'floors', FloorViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'models', ModelViewSet)
+router.register(r'uas', UaViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'uploads', FileUploadViewSet, basename='uploads')
 
+print(api_settings.DEFAULT_VERSION)
 urlpatterns = [ 
-    path('', views.index, name='index'),
+    url(r'^v1/', include((router.urls, 'v1'), namespace='v1')),
     ]
