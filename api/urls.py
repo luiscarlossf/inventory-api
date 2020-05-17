0e2f890b121fd5ad26f1bcb46af88934c7098116
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-05-16 23:41:14 -0300

    Altera definição da rota para v1.

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,12 @@ from rest_framework.urlpatterns import url, include
 from rest_framework import routers 
 from django.urls import path
 from django.views.generic import TemplateView
-from rest_framework.settings import api_settings
+from .utils import get_version_default
 from .views import BrandViewSet, CategoryViewSet, ComputerViewSet, EquipamentViewSet, \
     FloorViewSet, GroupViewSet, ModelViewSet, UaViewSet, UserViewSet, FileUploadViewSet
 
 #A versão padrão da API 
-DEFAULT_VERSION = str(api_settings.DEFAULT_VERSION)
+DEFAULT_VERSION = get_version_default()
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'brands', BrandViewSet)
@@ -23,7 +23,7 @@ router.register(r'uploads', FileUploadViewSet, basename='uploads')
 
 
 urlpatterns = [ 
-    url(r'^'+ DEFAULT_VERSION+'/', include((router.urls, DEFAULT_VERSION), namespace=DEFAULT_VERSION)),
+    url(r'^v1/', include((router.urls, 'v1'), namespace='v1')),
     path('redoc/', TemplateView.as_view(
         template_name='redoc.html',
         extra_context={'schema_local':'openapi/schema-'+DEFAULT_VERSION+'.yml'}
